e9983f9feaa82e9e54b1dd02987db1d5bb7bb61c
Santiago <santiagodias238@gmail.com>
2025-11-11 20:07:35 +0000

    Refactor structs and add user message tracking

--- structs.h
+++ structs.h
@@ -1,33 +1,38 @@
 #pragma once
 
-struct tagHora{
+#include "consts.h"
+
+struct tagHora {
 	int hora;
 	int minuto;
 	int segundo;
 };
 
-struct tagData{
+struct tagData {
 	int ano;
 	int mes;
 	int dia;
 };
 
-struct tagDataHora{
+struct tagDataHora {
 	struct tagData data;
 	struct tagHora horario;
 };
 
-struct tagUser{
-	char username[50+1];
-	char nome[50+1];
-	char password[50+1];
+struct tagMensagem {
+	char userOrigem[50 + 1];
+	char userDestino[50 + 1];
+	struct tagDataHora dataHora;
+	char assunto[50 + 1];
+	char corpoMensagem[50 + 1];
 };
 
-struct tagMensagem{
-	char userOrigem[50+1];
-	char userDestino[50+1];
-	struct tagDataHora dataHora;
-	char assunto[50+1];
-	char corpoMensagem[50+1];
+struct tagUser {
+	char username[50 + 1];
+	char nome[50 + 1];
+	char password[50 + 1];
+	int qtdMsg;
+	int msgNaoLidas;
+	struct tagMensagem mensagens[MAX_MSG];
 };
 
