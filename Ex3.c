3208c26b9f8764d54b0b63e6627df6e2dcfd0ad2
Santiago <santiagodias238@gmail.com>
2025-11-11 10:26:52 +0000

    Implement user login and menu navigation
    Added login functionality and user validation.

--- Ex3.c
+++ Ex3.c
@@ -8,39 +8,97 @@
 
 int main()
 {
-	struct tagUser users[MAX_USERS] = {"sa","System Administrator","sa"};
+	struct tagUser users[MAX_USERS] = {"sa","System Administrator","sa"};	//User admin guardado na posicao 0
 	int qtdUsers = 1;;
 	struct tagMensagem mensagens[MAX_MSG];
 	int qtdMensagem = 0;
+
+	/*Variaveis de Menu*/
 	int firstTask = 0;
+
+	/**/
+	char usernameLogin[50+1],passwordLogin[50+1];
+	int loginIndex = 0,loggedIn = 0;
 
 	while(1)
 	{
 		do
 		{
 			firstTask = BootMenu();
+			getchar();	//Remover \n
 
 			if(EntreAB(firstTask,0,1) == 0)
 			{
 				puts("Selecione uma opcao valida!");
-			}else if(firstTask == 1)	//Login
-			{
-				do
-				{
-
-				}while();
 			}
 		}while(EntreAB(firstTask,0,1) == 0);
 
-		if(firstTask == 0)
+		if(firstTask == 0)	//Terminar programa
 		{
 			break;
-		}
+		}else if(firstTask == 1)	//Login
+		{
+			printf("Indique o username: ");
+			scanf("%s",usernameLogin);
+			getchar();
+
+			loginIndex = SearchUser(users,qtdUsers,usernameLogin);
+
+			if(loginIndex == -1)	//Nao encontrou user
+			{
+				puts("Username nao existe!");
+			}else	//Encontrou user
+			{
+				do
+				{
+					puts("Digite 'quit' para sair!");
+					printf("Indique a password de %s: ",usernameLogin);
+					scanf("%s",passwordLogin);
+
+					if(PasswordCheck(passwordLogin,users[loginIndex].password) == 0)
+					{
+						puts("Password incorreta!");
+					}else if(strcmp("quit",passwordLogin) == 0)
+					{
+						puts("Saindo..");
+						loggedIn = 0;
+						break; 
+					}else if(PasswordCheck(passwordLogin,users[loginIndex].password) == 1)
+					{
+						loggedIn = 1;
+					}
+				}while(PasswordCheck(passwordLogin,users[loginIndex].password) == 0);
+
+			}
+		}	//Login task
+
+			if(loggedIn == 1)	//User conseguiu entrar na conta
+			{
+				if(loginIndex == 0)	//System administrator
+				{
+					do	//Ciclo menu do admin
+					{
+
+					}while();
+
+				}else	//Outro user
+				{
+					do	//Ciclo de um user normal
+					{
+
+					}while();
+
+				}
+
+			}
+
 
 		system("pause");
 		system("cls");
-	}
+
+		}	//Ciclo infinito
 
 	puts("Saindo...");
 	return 0;
-}
+} //main
+
