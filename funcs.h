be7da27005adbd6a4fba76791773185c7015c74d
Santiago <santiagodias238@gmail.com>
2025-11-11 10:27:32 +0000

    Refactor funcs.h to add PasswordCheck function

--- funcs.h
+++ funcs.h
@@ -3,19 +3,23 @@
 #include <string.h>
 #include "structs.h"
 
-int BootMenu(void);
+int BootMenu(void);	//Menu de login e terminar
 int EntreAB(int x,int min,int max);
 int SearchUser(struct tagUser users[],int qtdUsers,char username[50+1]);
-
+int PasswordCheck(char password[],char realPassword[]);
 
 int SearchUser(struct tagUser users[],int qtdUsers,char username[50+1])
 {
 	int i = 0;
 
-	for(i = 0;i < qtdUsers;i++)
+	for(i = 0;i < qtdUsers;i++)	//Percorrer todos os usernames
 	{
-
+		if(strupr(users[i].username) == strupr(username))	//Comparar case insensitive
+		{
+			return i;	//Retorna index do user no campo
+		}
 	}
+	return -1;	//Nao encontrou user
 }
 
 int BootMenu(void)
@@ -34,4 +38,13 @@ int BootMenu(void)
 int EntreAB(int x,int min,int max)
 {
 	return x >= min && x <= max;
+}
+
+int PasswordCheck(char password[],char realPassword[])
+{
+	if(strcmp(password,realPassword) == 0)	//Compara case sensitive
+	{
+		return 1;	//Password correta
+	}
+	return 0;	//Password nao esta correta
 }
