3e76ba9e73348322abcf6762e1565dd2c6b75058
Santiago <santiagodias238@gmail.com>
2025-11-13 17:20:56 +0000

    Fix case insensitive username comparison

--- accountFuncs.h
+++ accountFuncs.h
@@ -20,10 +20,17 @@ int PasswordCheck(char password[], char realPassword[])
 int SearchUser(struct tagUser users[], int qtdUsers, char username[50 + 1])
 {
 	int i = 0;
+	char aux1[50 + 1],aux2[50+1];
+
+	strcpy(aux2, username);
+	_strupr(aux2);
 
 	for (i = 0;i < qtdUsers;i++)	//Percorrer todos os usernames
 	{
-		if (_strupr(users[i].username) == _strupr(username))	//Comparar case insensitive
+		strcpy(aux1, users[i].username);
+		_strupr(aux1);
+
+		if (strcmp(aux1,aux2) == 0)	//Comparar case insensitive
 		{
 			return i;	//Retorna index do user no campo
 		}
